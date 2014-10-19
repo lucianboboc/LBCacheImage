82e381cf2d16bb62271303f94b85159483f3304e
Lucian Boboc <info@lucianboboc.com>
2014-10-19 00:23:49 -0700

    Initial Commit

---
+++ LBCacheImageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LBCacheImageDemo
+//
+//  Created by Lucian Boboc on 19/10/14.
+//  Copyright (c) 2014 Lucian Boboc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
